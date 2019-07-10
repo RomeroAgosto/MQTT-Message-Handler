8526089112dbcfc1b5efb275c594ed5a95002d96
Ricardo <jony.ricardo95@gmail.com>
2019-07-10 16:19:49 +0100

    Sub and Pub sync
    normal pub and sync sub that requires one thread per topic subscribed

--- src/Main.c
+++ src/Main.c
@@ -4,13 +4,15 @@
 #include "MQTTClient.h"
 
 #define ADDRESS     "tcp://localhost:1883"
-#define CLIENTID    "ExampleClientPub"
-#define TOPIC       "MQTT Examples"
-#define PAYLOAD     "Hello World!"
-#define QOS         1
+#define CLIENTID    "Handler"
 #define TIMEOUT     10000L
 
 volatile MQTTClient_deliveryToken deliveredtoken;
+
+MQTTClient client;
+MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+MQTTClient_message pubmsg = MQTTClient_message_initializer;
+MQTTClient_deliveryToken token;
 
 void delivered(void *context, MQTTClient_deliveryToken dt)
 {
@@ -44,26 +46,26 @@ void connlost(void *context, char *cause)
     printf("     cause: %s\n", cause);
 }
 
-int main(int argc, char* argv[])
+void initHandler()
 {
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
-    int rc;
+	 int rc;
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+	MQTTClient_create(&client, ADDRESS, CLIENTID,
+	MQTTCLIENT_PERSISTENCE_NONE, NULL);
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
 
-    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
+	MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered);
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-    {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(EXIT_FAILURE);
-    }
+	if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+	{
+		printf("Failed to connect, return code %d\n", rc);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void msgdelivery(char TOPIC[], char PAYLOAD[], int QOS)
+{
     pubmsg.payload = PAYLOAD;
     pubmsg.payloadlen = strlen(PAYLOAD);
     pubmsg.qos = QOS;
@@ -74,7 +76,38 @@ int main(int argc, char* argv[])
             "on topic %s for client with ClientID: %s\n",
             PAYLOAD, TOPIC, CLIENTID);
     while(deliveredtoken != token);
+
+}
+
+void topSub(char TOPIC[], int QOS)
+{
+	int ch;
+	printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+	           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+	    MQTTClient_subscribe(client, TOPIC, QOS);
+
+	do
+	{
+		ch = getchar();
+	} while(ch!='Q' && ch != 'q');
+
+
+}
+
+void topUnsub(char TOPIC[])
+{
+    MQTTClient_unsubscribe(client, TOPIC);
+}
+int main(int argc, char* argv[])
+{
+	initHandler();
+
+	//pthread_t thread_id;
+	//pthread_create(&thread_id, NULL,topSub, "MQTT Message", 1);
+
+    msgdelivery("MQTT Examples", "Hello World!", 1);
+    while(1);
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
-    return rc;
+    return 0;
 }
